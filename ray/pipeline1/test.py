ab6d63be30b6f344b5968a906c04548597736f25
jamalh11 <jamalh11@node9.gojiexp.goji-pg0.wisc.cloudlab.us>
2025-03-31 23:09:36 +0000

    update hosts

--- ray/pipeline1/test.py
+++ ray/pipeline1/test.py
@@ -29,12 +29,10 @@ LOGGING_DIR = "/users/jamalh11/raylogs"
 DATA_DIR = "/mydata"
 
 hosts = [
-        "http://10.10.1.13:8000",
-        "http://10.10.1.12:8000",
-        "http://10.10.1.11:8000",
         "http://10.10.1.10:8000",
         "http://10.10.1.9:8000",
-        "http://10.10.1.8:8000"
+        "http://10.10.1.8:8000",
+        "http://10.10.1.7:8000",
     ]
 session = None
 
